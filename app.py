446fc6ce55dcf8ee0bd2070c1037f9f27ec60e78
Jennifer <jennifer.kouassi@edu.ece.fr>
2025-01-02 04:51:47 +0100

    fix: app.py
    add !
    
    Co-Authored-By: octopushe <193452760+octopushe@users.noreply.github.com>

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def hello():
-    return 'Hello World, this my first app with flask.'
+    return 'Hello World, this my first app with flask!'
 
 if __name__ == '__main__': 
     app.run(debug=True)
